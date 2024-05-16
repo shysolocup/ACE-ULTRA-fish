246cf3d2695b44ef2b267913d6a3db3f19b2287f
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-05-16 08:39:42 -0500

    Added some more file extension types

--- API/fileExtTypes.js
+++ API/fileExtTypes.js
@@ -10,7 +10,12 @@ API.newP("fileExtTypes", function() {
         [`.skin.${ext}`]: this.types.Skin,
         [`.texture.${ext}`]: this.types.Texture,
         [`.sound.${ext}`]: this.types.Sound,
-        [`.wrap.${ext}`]: this.types.Wrap
+        [`.wrap.${ext}`]: this.types.Wrap,
+        [`.model.${ext}`]: this.types.Model,
+        [`.color.${ext}`]: this.types.Color,
+        [`.position.${ext}`]: this.types.Vector3,
+        [`.rotation.${ext}`]: this.types.Vector3,
+        [`.cframe.${ext}`]: this.types.CFrame
     
     }
     
